cb8a3eea2a2388a12c7d88fe00bb32d7a266efb7
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-07-19 06:02:34 +0200

    download artifact to ensure the _site is here

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -7,4 +7,5 @@ BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 export WEBSERVER=es15.siteground.eu
 
 echo "--- Deploying $PROJECT..."
+buildkite-agent artifact download
 rsync -avz _site/ $WEBSERVER:~/public_html/$PROJECT
