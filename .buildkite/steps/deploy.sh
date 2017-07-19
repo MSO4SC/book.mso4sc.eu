a0dd9c4e9495d99bffb6ac5d0eedd66e2f196b87
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-07-19 06:10:49 +0200

    up

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -7,5 +7,5 @@ BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 export WEBSERVER=es15.siteground.eu
 
 echo "--- Deploying $PROJECT..."
-buildkite-agent artifact download site.tar.gz .
+buildkite-agent artifact download site.tar.gz . --build ${BUILDKITE_BUILD_ID}
 rsync -avz _site/ $WEBSERVER:~/public_html/$PROJECT
