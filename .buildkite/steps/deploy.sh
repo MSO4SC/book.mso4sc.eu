8432dcfa5e5b06cff4f8328d122e0e44e56ab250
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-07-19 05:38:51 +0200

    fix

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 set -x
 
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
+export WEBSERVER=es15.siteground.eu
 
 echo "--- Deploying $PROJECT..."
 rsync -avz _site/ $WEBSERVER:~/public_html/$PROJECT
